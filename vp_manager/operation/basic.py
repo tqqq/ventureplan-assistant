0e3450bc574514364474d90aac28b312780a1f0d
tqqq <qing.tong@wiz.ai>
2021-09-16 14:31:50 +0800

    鼠标键盘资源上锁

--- vp_manager/operation/basic.py
+++ vp_manager/operation/basic.py
@@ -1,32 +1,49 @@
 # coding=utf-8
 
+import threading
+
 """
     basic mouse/keyboard operation
 """
 
 
-def mouse_move(x, y):
-    # TODO
-    pass
+class DeviceManager:
+    def __init__(self):
+        self.lock = threading.Lock()
 
+    def __enter__(self):
+        self.lock.acquire()
+        return self
 
-def mouse_click(x, y):
-    # TODO
-    pass
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.lock.release()
 
+    def mouse_move(self, x, y):
+        # TODO
+        pass
 
-def mouse_right_click(x, y):
-    # TODO
-    pass
+    def mouse_click(self, x, y):
+        # TODO
+        pass
 
+    def mouse_right_click(self, x, y):
+        # TODO
+        pass
 
-def kboard_click(key):
-    # TODO
-    pass
+    def mouse_scroll_up(self, x, y):
+        # TODO
+        pass
 
+    def mouse_scroll_down(self, x, y):
+        # TODO
+        pass
 
-def kboard_combine_click(key1, key2):
-    # TODO
-    pass
+    def kboard_click(self, key):
+        # TODO
+        pass
+
+    def kboard_combine_click(self, key1, key2):
+        # TODO
+        pass
 
 
